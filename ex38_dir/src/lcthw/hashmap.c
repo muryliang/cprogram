fe8a2695856408f59ce139a463e90ed957c88a60
muryliang <muryliang@gmail.com>
2015-09-15 23:16:57 +0800

    doing hash

--- ex38_dir/src/lcthw/hashmap.c
+++ ex38_dir/src/lcthw/hashmap.c
@@ -105,4 +105,94 @@ error:
 	return NULL;
 }
 
+int Hashmap_set( Hashmap *map , void* key , void *data)
+{
+	uint32_t hash = 0;
+	Darray *bucket = Hashmap_find_bucket(map , key , 1 , &hash);
+	check(bucket , "error can't create bucket");
 
+	HashmapNode *node = Hashmap_node_create(hash , key , data);
+	check_mem( node );
+
+	DArray_push( bucket , node );
+
+	return 0;
+error:
+	return -1;
+}
+
+static inline int Hashmap_get_node( Hashmap *map , uint32_t hash , DArray *bucket , void*key)
+{
+	int i =0;
+
+	for( i = 0; i < DArray_end(bucket);i++)
+	{
+		debug("try: %d" , i);
+		HashmapNode *node = DArray_get(bucket , i);
+		if( node->hash == hash && map->compare(node->key , key) == 0){
+			return i;
+		}
+	}
+	return -1;
+}
+
+void *Hashmap_get(Hashmap *map , void *key)
+{
+	uint32_t hash = 0;
+	DArray *bucket = Hashmap_find_bucket(map , key , 0 , &hash);
+	if(!bucket) return NULL;
+
+	int i = Hashmap_get_node(map, hash , bucket , key);
+	if( i == -1) return NULL;
+
+	HashmapNode *node = DArray_get(bucket , i);
+	check(node != NULL , "failed to get node form bucket when it should exist");
+
+	return node->data;
+error:
+	return NULL;
+}
+
+
+int Hashmap_traverse(Hashmap *map , Hashmap_traverse_cb traverse_cb)
+{
+	int i = 0;
+	int j = 0;
+	int rc = 0;
+
+	for(i = 0 ; i< DArray_count(map->buckets);i++){
+		DArray *bucket = DArray_get(map->buckets , i);
+		if(buckets)
+		{
+			for(j = 0 ; j < DArray_count(bucket);j++)
+			{
+				HashmapNode *node = DArray_get(bucket , j);
+				rc = traverse_cb(node);
+				if(rc != 0 )return rc;
+			}
+		}
+	}
+	return 0;
+}
+
+void *Hashmap_delete(Hashmap *map , void *key)
+{
+	uint32_t hash = 0;
+	DArray *bucket = Hashmap_find_bucket(map , key , 0 , &hash);
+	if( !bucket) return NULL;
+
+	int i = Hashmap_get_node(map , hash ,bucket , key);
+	if( i == -1) return NULL;
+
+	HashmapNode *node = DArray_get(bucket , i);
+	void *data = node->data;
+
+	HashmapNode *ending = DArray_pop(bucket);
+
+	if(ending != node){
+		DArray_set(bucket , i , ending);
+	}
+
+	free(node);
+	return data;
+}
